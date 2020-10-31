93929e7f6579957bad2da4800d814ade82c31efe
DwiRizqi0 <73012562+DwiRizqi0@users.noreply.github.com>
2020-10-31 16:20:02 +0800

    Add project files.

---
+++ Client-Dwi Rizqi Oktaviani-20180140111/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_Dwi_Rizqi_Oktaviani_20180140111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
